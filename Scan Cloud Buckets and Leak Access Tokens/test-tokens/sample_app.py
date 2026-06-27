5e0b8bd4a85ac19c6d4a7e94ce5a448257087a22
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-06-26 20:37:29 -0700

    Create sample_app.py

---
+++ Scan Cloud Buckets and Leak Access Tokens/test-tokens/sample_app.py
@@ -0,0 +1,7 @@
+# Bad practice example - hardcoded credentials
+AWS_KEY = "AKIAIOSFODNN7EXAMPLE"
+SECRET = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+def connect():
+    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIn0.dozjgNryP4J3jVmNHl0w5N_XgL0n3I9PlFUP0THsR8U"
+    return token
